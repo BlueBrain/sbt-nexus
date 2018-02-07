aee181b6b808a3b840b35c2191bddc7e7a319772
Henry <hygt@users.noreply.github.com>
2018-02-07 16:53:07 +0100

    Kamon 1.0.1 (#28)
    * Upgrade to Kamon 1.0.1
    
    * Update kamon dependencies

--- src/main/scala/ch/epfl/bluebrain/nexus/sbt/nexus/MonitoringPlugin.scala
+++ src/main/scala/ch/epfl/bluebrain/nexus/sbt/nexus/MonitoringPlugin.scala
@@ -11,13 +11,13 @@ object MonitoringPlugin extends AutoPlugin {
 
   override lazy val projectSettings = Seq(
     libraryDependencies ++= Seq(
-      "io.kamon" %% "kamon-core"            % "1.0.0",
+      "io.kamon" %% "kamon-core"            % "1.0.1",
       "io.kamon" %% "kamon-prometheus"      % "1.0.0",
       "io.kamon" %% "kamon-jaeger"          % "1.0.1",
+      "io.kamon" %% "kamon-system-metrics"  % "1.0.0",
       "io.kamon" %% "kamon-akka-http-2.5"   % "1.0.1",
-      "io.kamon" %% "kamon-akka-2.5"        % "1.0.0" % Runtime,
-      "io.kamon" %% "kamon-akka-remote-2.5" % "1.0.0" % Runtime,
-      "io.kamon" %% "kamon-system-metrics"  % "1.0.0" % Runtime
+      "io.kamon" %% "kamon-akka-2.5"        % "1.0.1" % Runtime,
+      "io.kamon" %% "kamon-akka-remote-2.5" % "1.0.0" % Runtime
     )
   )
 }
